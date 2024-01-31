01015926d03b86784de1ad465d1d522f34016521
Kunal Sharma <cse.190320027@silicon.ac.in>
2024-02-01 01:20:14 +0530

    learning javascript
    Written a program to make a terminal clock

---
+++ abc.js
@@ -0,0 +1,15 @@
+function updateClock()
+{
+    const now=new Date();
+    const hours = now.getHours().toString().padStart(2,'0');
+    const minutes = now.getMinutes().toString().padStart(2,'0');
+    const seconds = now.getSeconds().toString().padStart(2,'0');
+
+    //process.stdout.clearLine();
+    process.stdout.cursorTo(0);
+    process.stdout.write(`${hours}:${minutes}:${seconds}`);
+    setTimeout(updateClock,1000);
+
+}
+
+updateClock();
